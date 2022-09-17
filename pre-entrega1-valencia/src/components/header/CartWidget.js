fee06027b6ae175e7b188b83a2fd6556dc881514
JuanCruzValencia <jcvalencia@ismt.edu.ar>
2022-09-17 19:52:21 -0300

    cart-count

--- pre-entrega1-valencia/src/components/header/CartWidget.js
+++ pre-entrega1-valencia/src/components/header/CartWidget.js
@@ -1,9 +1,14 @@
-//este va a ser el carrito, por ahora solo renderizar un icono de carrito
 import { MdOutlineShoppingBag } from "react-icons/md";
+import './CartWidget.css'
 
 function CartWidget() {
   return (
-      <a href='#cart'><MdOutlineShoppingBag /></a>
+    <>
+      <a href="#cart">
+        <MdOutlineShoppingBag />
+      </a>
+      <span className="cart__count">3</span>
+    </>
   );
 }
 
